453330e5da474574306b82517b0d6ba7583abd36
luroel <jl.rodrigueze@hotmail.com>
2025-06-11 17:00:06 -0600

    Correcciones de test

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -7,7 +7,7 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = df['race'].value_counts().tolist()
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
     average_age_men = round(df[df['sex']=='Male']['age'].mean(), 1)
@@ -72,5 +72,3 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
-    
-calculate_demographic_data()
