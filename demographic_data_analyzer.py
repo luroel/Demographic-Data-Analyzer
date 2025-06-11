d33426bb22451b62f65ca35f4933f9e4067ec250
luroel <jl.rodrigueze@hotmail.com>
2025-06-11 13:38:49 -0600

    Minimo de horas trabajadas por semana min_work_hours

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -29,7 +29,7 @@ def calculate_demographic_data(print_data=True):
 & (df['salary'] == '>50K')).sum())*(100))/(lower_education), 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = None
